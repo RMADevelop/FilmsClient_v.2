40d3e92a6bedf1731299d3594630ada565c0a71b
RomanM <arct1k77@gmail.com>
2017-10-27 06:58:10 +0300

    fix bug with module

--- app/src/main/java/com/example/romanm/filmsclientv2/App.java
+++ app/src/main/java/com/example/romanm/filmsclientv2/App.java
@@ -5,6 +5,9 @@ import android.app.Application;
 import com.example.romanm.filmsclientv2.di.component.AppComponent;
 import com.example.romanm.filmsclientv2.di.component.DaggerAppComponent;
 import com.example.romanm.filmsclientv2.di.modules.ContextModule;
+import com.example.romanm.filmsclientv2.di.modules.RemoteDataModule;
+
+import static com.example.romanm.filmsclientv2.utils.Const.URL_TMDb;
 
 /**
  * Created by RomanM on 25.10.2017.
@@ -20,6 +23,7 @@ public class App extends Application {
 
         appComponent = DaggerAppComponent.builder()
                 .contextModule(new ContextModule(this))
+                .remoteDataModule(new RemoteDataModule(URL_TMDb))
                 .build();
 
 
